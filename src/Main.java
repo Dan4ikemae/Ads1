481a41784c15b5258dafd686ec55f59bc74e84d9
Dan4ikemae <240758@astanait.edu.kz>
2025-03-30 23:13:19 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {10, 1, 32, 3, 45};
-
-        long startTime = System.nanoTime(); 
-
-        int min = findMin(numbers);
-
-        long endTime = System.nanoTime(); 
-        double timeTaken = (endTime - startTime) / 1e6; 
-
+        long start = System.nanoTime(); 
+        int min = fmin(numbers);
+        long end = System.nanoTime(); 
+        double timeTaken = (end - start) / 1e6; 
         System.out.println("Minimum value is: " + min);
         System.out.println("Time taken: " + timeTaken + " milliseconds");
     }
-
-    public static int findMin(int[] arr) {
+    public static int fmin(int[] arr) {
         int min = arr[0];
         for (int num : arr) {
             if (num < min) {
@@ -23,5 +18,4 @@ public class Main {
         }
         return min;
     }
-
 }
