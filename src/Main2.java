c6b2998e6b1e7ac039bef0c473e3ba84c73838e5
Dan4ikemae <240758@astanait.edu.kz>
2025-03-30 23:30:59 +0500

    Update Main2.java

--- src/Main2.java
+++ src/Main2.java
@@ -1,10 +1,10 @@
 public class Main2 {
     public static void main(String[] args) {
         int[] numbers = {3, 2, 4, 1};
-        long start = System.nanoTime(); //  начало времени
+        long start = System.nanoTime();
         double avg = favg(numbers);
-        long end = System.nanoTime(); // конец времени
-        double timeTaken = (end - start) / 1e6; // поменять на милисекунды
+        long end = System.nanoTime();
+        double timeTaken = (end - start) / 1e6;
         System.out.println("Avg value is: " + avg);
         System.out.println("Time taken: " + timeTaken + " milliseconds");
     }
